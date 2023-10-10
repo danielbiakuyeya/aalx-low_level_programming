77d84407b99efffaae520c4e3d6b844b2c90bb6b
danielbiakuyeya <danielbiakuyeya123@gmail.com>
2023-10-09 21:32:13 -0700

    All Task

---
+++ 0x0E-structures_typedef/1-main.c
@@ -0,0 +1,12 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct dog my_dog;
+
+    init_dog(&my_dog, Poppy, 3.5, Bob);
+    return (0);
+}
---
+++ 0x0E-structures_typedef/4-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include dog.h
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    dog_t *my_dog;
+
+    my_dog = new_dog(Poppy, 3.5, Bob);
+    return (0);
+}
